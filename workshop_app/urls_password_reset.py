836329cb81f1782d26446d5d4ed59f65301f2122
summar22 <summarporwal22@gmail.com>
2026-04-05 22:37:36 +0530

    updated password reset file

--- workshop_app/urls_password_reset.py
+++ workshop_app/urls_password_reset.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.contrib.auth.views import password_reset, password_reset_confirm,\
         password_reset_done, password_reset_complete, password_change,\
         password_change_done
